3089ad37b3cc2c084a5176d83990e918d6796c86
Curious Narwhal <96975131+eightteababi@users.noreply.github.com>
2024-11-05 16:31:58 +1100

    Add files via upload

---
+++ ui/src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const user = true;
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>MelbourneHousing</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">Charts</a>
+        <a href="/list">Available Properties</a>
+        <a href="/">About Us</a>
+      </div>
+      <div className="right">
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ ui/src/routes/homePage/homePage.jsx
@@ -0,0 +1,37 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Find the dream house in Melbourne right within your range!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>1</h1>
+              <h2>Year of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200+</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>30,000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
